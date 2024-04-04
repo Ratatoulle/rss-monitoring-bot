b5bc92ea9cbded900db06b48d049f38d0e49aa55
Andrew <1vasilev.andrey@gmail.com>
2024-04-04 16:17:33 +0500

    add docstrings for states.py

--- bot/core/utils/states.py
+++ bot/core/utils/states.py
@@ -2,11 +2,17 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class AddResource(StatesGroup):
+    """
+        Class represents group of states when user wants to add resource
+    """
     rss_url = State()
     is_rss_valid = State()
 
 
 class GetNews(StatesGroup):
+    """
+        Class represents group of states when user wants to get news from resource(s)
+    """
     all_or_separately = State()
     choose_resource = State()
     delta = State()
